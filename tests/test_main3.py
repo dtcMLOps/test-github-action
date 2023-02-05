b0752f249dc293d04ee30cdf49e9261b1eb2758d
Daniel Tobon Collazos <daniel.tobon@ab-inbev.com>
2023-02-05 14:39:10 -0500

    AB#0 fix: sadsdsdsd.

---
+++ tests/test_main3.py
@@ -0,0 +1,18 @@
+def test_main_function():
+    assert True
+
+
+def test_main_function2():
+    assert True
+
+
+def test_main_function3():
+    assert True
+
+
+def test_main_function4():
+    assert True
+
+
+def test_main_function5():
+    assert True
